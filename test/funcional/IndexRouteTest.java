f367cce15a0a07ab0a0b94a8c722d43ab4f27c2e
claudivanfilho <j.claudivan.filho@gmail.com>
2014-06-13 16:29:27 -0300

    melhorar teste de routes

--- test/funcional/IndexRouteTest.java
+++ test/funcional/IndexRouteTest.java
@@ -1,5 +1,6 @@
 package funcional;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import play.mvc.Result;
@@ -9,31 +10,27 @@ import play.test.Helpers;
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.running;
+import static play.test.Helpers.inMemoryDatabase;
+import static play.test.Helpers.start;
 
 public class IndexRouteTest {
 
+	@Before
+	public void setUp() throws Exception {
+		start(fakeApplication(inMemoryDatabase()));
+	}
+
 	@Test
 	public void rootRoute() {
-		running(fakeApplication(), new Runnable() {
-			@Override
-			public void run() {
-				Result result = Helpers.route(new FakeRequest(GET, "/"));
-				// testa se a resultado da requisição à url "/" não é nula
-				assertThat(result).isNotNull();
-			}
-		});
+		Result result = Helpers.route(new FakeRequest(GET, "/"));
+		// testa se a resultado da requisição à url "/" não é nula
+		assertThat(result).isNotNull();
 	}
 
 	@Test
 	public void badRoute() {
-		running(fakeApplication(), new Runnable() {
-			@Override
-			public void run() {
-				Result result = Helpers.route(new FakeRequest(GET, "/bad"));
-				// com não existe uma route "/bad" o resultado deve ser nulo
-				assertThat(result).isNull();
-			}
-		});
+		Result result = Helpers.route(new FakeRequest(GET, "/bad"));
+		// com não existe uma route "/bad" o resultado deve ser nulo
+		assertThat(result).isNull();
 	}
 }
